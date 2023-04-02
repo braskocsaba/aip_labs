3f67dbeee224d20e76c9f1c45d3e97b12b19e5cd
Brasko Csaba <brasko.csaba@gmail.com>
2023-04-02 05:00:56 +0000

    add db session manager

---
+++ backend/config.py
@@ -0,0 +1,7 @@
+import os
+
+DB_HOST = os.environ.get('DB_HOST', 'localhost')
+DB_PORT = os.environ.get('DB_PORT', '5432')
+DB_NAME = os.environ.get('DB_NAME', 'aip_labs')
+DB_USER = os.environ.get('DB_USER', 'local_user')
+DB_PASS = os.environ.get('DB_PASS', 'pwd12345')
