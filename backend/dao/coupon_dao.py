c30046d7a21f91ffeaa3ba50efb21b092a3b8f99
Brasko Csaba <brasko.csaba@gmail.com>
2023-04-03 14:36:02 +0000

    add endpoint and dao to set coupon to used

--- backend/dao/coupon_dao.py
+++ backend/dao/coupon_dao.py
@@ -4,23 +4,36 @@ from typing import List, Union
 from logger import logger
 
 
-def get_all_coupons_from_db(db: Session) -> Union[List[Coupon], None]:
+def get_all_coupons_from_db(session: Session) -> Union[List[Coupon], None]:
     try:
         statement = select(Coupon)
-        result = db.exec(statement).all()
+        coupons = session.exec(statement).all()
         logger.info(f"Succeed to retrieve all coupons")
-        return result
+        return coupons
     except Exception as e:
         logger.error(f"Failed to retrieve all coupons, error: {e}")
         return None
 
 
-def get_coupons_from_db(db: Session, id:int) -> Union[List[Coupon], None]:
+def get_coupon_from_db(session: Session, coupon_id: int) -> Union[Coupon, None]:
     try:
-        statement = select(Coupon).where(Coupon.id == id)
-        result = db.exec(statement).one_or_none()
-        logger.info(f"Succeed to retrieve the coupon with id")
-        return result
+        statement = select(Coupon).where(Coupon.id == coupon_id)
+        coupon = session.exec(statement).first()
+        logger.info(f"Succeed to retrieve the coupon with id: {coupon_id}")
+        return coupon
     except Exception as e:
-        logger.error(f"Failed to retrieve all coupons, error: {e}")
+        logger.error(f"Failed to retrieve the coupon with id: {coupon_id}, error: {e}")
+        return None
+
+
+def set_coupon_used_in_db(session: Session, coupon_id: int) -> Union[Coupon, None]:
+    try:
+        statement = select(Coupon).where(Coupon.id == coupon_id)
+        coupon = session.exec(statement).first()
+        coupon.used = True
+        session.commit()
+        logger.info(f"Succeed set the coupon with id id: {coupon_id} to used")
+        return coupon
+    except Exception as e:
+        logger.error(f"Failed to set the coupon with id id: {coupon_id} to used, error: {e}")
         return None
--- backend/handlers/coupon_handler.py
+++ backend/handlers/coupon_handler.py
@@ -2,7 +2,8 @@ from db import session_manager
 from models import Coupon
 from sqlmodel import select
 from typing import List, Union
-from dao.coupon_dao import get_all_coupons_from_db
+from dao.coupon_dao import get_all_coupons_from_db, get_coupon_from_db, set_coupon_used_in_db
+
 
 def get_all_coupons() -> Union[List[Coupon], None]:
     """
@@ -12,16 +13,17 @@ def get_all_coupons() -> Union[List[Coupon], None]:
         return get_all_coupons_from_db(session)
 
 
-def get_coupon(coupon_id: int) -> dict:
+def get_coupon(coupon_id: int) -> Union[Coupon, None]:
     """
     Get specific coupon stored in the database
     """
-    pass
+    with session_manager() as session:
+        return get_coupon_from_db(session, coupon_id)
 
 
-def create_coupon() -> dict:
+def set_coupon(coupon_id: int) -> Union[Coupon, None]:
     """
-    create coupon from data in the parameter and store in the database
+    Set specific coupon to used and store it in the database
     """
-    pass
-
+    with session_manager() as session:
+        return set_coupon_used_in_db(session, coupon_id)
