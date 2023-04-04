4e0bcfc44a296e3db8d8992967b2ecc0372997cb
Brasko Csaba <brasko.csaba@gmail.com>
2023-04-04 15:04:24 +0000

    add Action to models.py

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
@@ -6,16 +6,15 @@ from typing import Optional
 class Coupon(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     used: bool
+    action_id: int = Field(default=None, foreign_key="action.id")
+
 
 class Action(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     type: str = Field(nullable=False)
     name: str = Field(nullable=False)
     description: str = Field(nullable=True)
-    # valid_until: datetime = Field(default_factory=datetime.datetime.utcnow, nullable=False)
+    valid_until: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     discount_percent: int = Field(nullable=True)
     discount_value_currency: str = Field(nullable=True)
     discount_value: int = Field(nullable=True)
-
-
-
